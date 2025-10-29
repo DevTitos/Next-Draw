6285be53f55fac3f079960114c9615327253a2bc
DevTitos <kipkoechtitos@gmail.com>
2025-10-29 22:44:40 +0300

    static files config update

--- NextStar/settings.py
+++ NextStar/settings.py
@@ -107,7 +107,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
